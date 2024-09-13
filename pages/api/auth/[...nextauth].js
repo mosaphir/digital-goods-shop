74e7b94e53e3051bac48e48bc877dc80be8635e7
Saikot Hasan <skdm0111@gmail.com>
2024-09-14 02:19:45 +0600

    Create [...nextauth].js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  providers: [
+    Providers.Credentials({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      authorize: async (credentials) => {
+        // Add your own logic here to find the user from the database
+        const user = { id: 1, name: "User", email: "user@example.com" };
+        if (user) {
+          return Promise.resolve(user);
+        } else {
+          return Promise.resolve(null);
+        }
+      }
+    })
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+    newUser: null
+  },
+  session: {
+    jwt: true
+  },
+  callbacks: {
+    async jwt(token, user) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session(session, token) {
+      session.user.id = token.id;
+      return session;
+    }
+  }
+});
